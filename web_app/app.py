0b4db1cdf2449cea30afd483a3e3ef4007c98778
dunkelweizen <53185634+dunkelweizen@users.noreply.github.com>
2020-02-17 22:02:34 -0500

    added functionality to see user's most interactive tweets

--- web_app/app.py
+++ web_app/app.py
@@ -5,7 +5,6 @@ import tweepy
 from dotenv import load_dotenv
 import os
 import basilica
-import sqlite3
 
 from sqlalchemy.sql.functions import user
 from tweepy import TweepError
@@ -39,13 +38,22 @@ class Tweet(db.Model):
 auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 client = tweepy.API(auth)
-
-elon_tweets = client.user_timeline("elonmusk", tweet_mode="extended")
-for tweet in elon_tweets:
-    print(tweet.full_text)
-one_tweet = client.user_timeline('elonmusk', tweet_mode='extended', count=1)
-print(one_tweet[0].full_text)
-
+@app.route("/interactive_tweets", methods=["POST"])
+def interactive_tweets():
+    username = request.form['name']
+    friends = client.friends(username)
+    highest_count = 0
+    most_interacted_list = []
+    for friend in friends:
+        tweets = client.user_timeline(friend.screen_name, tweet_mode='extended')
+        for tweet in tweets:
+            interactions = tweet.retweet_count + tweet.favorite_count
+            if interactions >= highest_count:
+                most_interacted = str(tweet.user.screen_name) + ':' + str(tweet.full_text)
+                highest_count = interactions
+        most_interacted_list.append(most_interacted)
+        highest_count = 0
+    return jsonify(most_interacted_list)
 
 @app.route("/")
 def index():
@@ -66,8 +74,7 @@ def users():
 
 @app.route("/users/create", methods=["POST"])
 def create_user():
-    conn = sqlite3.connect('web_app_cai_nowicki.db')
-    c = conn.cursor()
+    db.session.commit()
     print("CREATING A NEW USER...")
     print("FORM DATA:", dict(request.form))
     name = request.form["name"]
@@ -77,24 +84,23 @@ def create_user():
         try:
             client.get_user(name)
             tweets = client.user_timeline(name, tweet_mode="extended")
-            query = "SELECT id FROM user WHERE name = %s" % name
-            #the dynamic string here doesn't work, try something else
-            user_id = c.execute(query).fetchone()
-            print(user_id)
-            db.session.add(Tweet(user_id=user_id, status=tweets[0].full_text))
+            user = User.query.filter_by(name=name).first()
+            db.session.add(Tweet(user_id=user.id, status=tweets[0].full_text))
+            db.session.commit()
         except TweepError:
             return render_template('error_new_user.html')
-        db.session.commit()
         print(jsonify({"message": "CREATED OK", "name": name}))
         return render_template('new_user_created.html')
     else:
         print(jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"}))
         return render_template('error_new_user.html')
 
-users = User.query.all()  # returns a list of <class 'alchemy.User'>
-
-users_response = []
-for u in users:
-    user_dict = u.__dict__
-    del user_dict["_sa_instance_state"]
-    users_response.append(user_dict)
+@app.route("/tweets")
+def tweets():
+    tweets = Tweet.query.all()
+    tweets_list = []
+    for tweet in tweets:
+        tweet_dict = tweet.__dict__
+        del tweet_dict["_sa_instance_state"]
+        tweets_list.append(tweet_dict)
+    return jsonify(tweets_list)
